27d00cd8c19ba04770a8fd1550a92b2a2dbb0e91
akswosn <akswosn@gmail.com>
2018-12-28 14:25:53 +0900

    first

---
+++ frontend/src/components/IndexPage.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+
+<div>
+    <h1>인덱스 페이지 입니다.!!!!</h1>
+    <hr/>
+    <router-link to="/movie">Movie Example</router-link>
+</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ frontend/src/components/LoginPage.vue
@@ -0,0 +1,17 @@
+<template lang="html">
+<div>
+    <h1>로그인 페이지입니다.</h1>
+    <hr/>
+    <a href="/movie">Movie Example</a>
+    <router-link to="/movie">Movie Example</router-link>
+</div>
+</template>
+
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
