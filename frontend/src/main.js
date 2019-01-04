bf4cc8e66ae8db83ff2827d39fcb0b7069acbe08
akswosn <akswosn@gmail.com>
2019-01-04 10:49:17 +0900

    console.log 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,6 +14,20 @@ Vue.prototype.$http = axios
 
 Vue.use(BootstrapVue)
 /* eslint-disable no-new */
+var styles = [
+  'background: linear-gradient(#D33106, #571402)'
+  , 'border: 1px solid #3E0E02'
+  , 'color: white'
+  , 'display: block'
+  , 'text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)'
+  , 'box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset'
+  , 'line-height: 40px'
+  , 'text-align: center'
+  , 'font-weight: bold'
+].join(';');
+
+console.log('%c[akswosn@gmail.com] forlks project vuejs + express + mongodb '  , styles);
+
 new Vue({
   el: '#app',
   router,
